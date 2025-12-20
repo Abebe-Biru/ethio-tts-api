e7ad6f41d225535d09aa5e9d441d1ac35ff8032b
Abebe Biru <daraaraabirruu@gmail.com>
2025-12-20 11:45:32 +0300

    Add setuptools installation to fix distutils ModuleNotFoundError

--- install_cpu_torch.bat
+++ install_cpu_torch.bat
@@ -4,6 +4,9 @@ REM Script to install CPU-only PyTorch on Windows
 REM Install uv if not already installed
 pip install uv
 
+REM Install setuptools for Python 3.10
+pip install setuptools
+
 REM Install CPU-only PyTorch first
 pip install torch==2.9.1+cpu torchvision==0.14.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 
--- install_cpu_torch.sh
+++ install_cpu_torch.sh
@@ -4,6 +4,9 @@
 # Install uv if not already installed
 pip install uv
 
+# Install distutils for Python 3.10
+pip install setuptools
+
 # Install CPU-only PyTorch first
 pip install torch==2.9.1+cpu torchvision==0.14.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 
